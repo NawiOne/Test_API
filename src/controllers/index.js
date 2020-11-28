604a667b53a3d018019c5d6e3a75ad68fd1998fb
shadwoe <rudinnawi@gmail.com>
2020-11-28 23:33:46 +0700

    commit

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -3,6 +3,24 @@ const dummyUser = require("../../dummyUser");
 const excelToJson = require("convert-excel-to-json");
 
 const mainController = {
+  primes: (req, res) => {
+    const { num } = req.query;
+    let primesArr = [];
+    let max = num + 100;
+    for (let i = Number(num) + 1; i <= max; i++) {
+      let count = 0;
+      for (let j = 2; j < i; j++) {
+        if (i % j === 0) {
+          count = 1;
+          break;
+        }
+      }
+      if (i > 1 && count === 0) {
+        primesArr.push(i);
+      }
+    }
+    res.json(primesArr.slice(0, 20));
+  },
   insertToExcel: async (req, res) => {
     const { id, email, firstName, lastName, avatar } = req.query;
     const user = {
--- src/routers/index.js
+++ src/routers/index.js
@@ -1,7 +1,8 @@
 const Router = require('express').Router()
 const mainController = require('../controllers/index')
 
-Router.post('/', mainController.insertToExcel)
-Router.get('/', mainController.readExcel)
+Router.post('/user', mainController.insertToExcel)
+Router.get('/user', mainController.readExcel)
+Router.get('/primes', mainController.primes)
 
 module.exports = Router;
